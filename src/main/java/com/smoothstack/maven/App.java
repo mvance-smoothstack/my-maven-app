2635b3c0ccd0c44f59a2a7c2360cab24dd3635d7
mvance-smoothstack <77799438+mvance-smoothstack@users.noreply.github.com>
2021-02-02 14:09:07 -0500

    initial commit

---
+++ src/main/java/com/smoothstack/maven/App.java
@@ -0,0 +1,13 @@
+package com.smoothstack.maven;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World! (But from inside Eclipse this time.)" );
+    }
+}
